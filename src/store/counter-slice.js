7bd7dd03cbecaf2d83e188d2ad10a7f42d5bb807
kjh-busan <picsmash7@gmail.com>
2023-04-09 23:46:19 +0900

    feat: distribute slice:

---
+++ src/store/counter-slice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialCounterState = {
+  counter: 0,
+  showCounter: true,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increnent(state) {
+      state.counter++;
+    },
+    decrenent(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+export default counterSlice;
+export const counterActions = counterSlice.actions;
--- src/store/index.js
+++ src/store/index.js
@@ -1,51 +1,10 @@
 // const redux = require("redux");
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const initialCounterState = {
-  counter: 0,
-  showCounter: true,
-};
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increnent(state) {
-      state.counter++;
-    },
-    decrenent(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counter-slice";
+import authSlice from "./auth-slice";
 
 const store = configureStore({
   reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 export default store;
